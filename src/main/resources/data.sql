bbb5aa79521f86ce6278fc9cf0ef923a5b75c4b9
Fabiola <fabirato09@gmail.com>
2021-12-01 14:14:02 -0600

    se agregan cambios al data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -51,3 +51,24 @@ SET @curso_id = LAST_INSERT_ID();
 INSERT INTO curso (ciclo, creditos, electivo, horas_laboratorio, horas_practica, horas_teoria, horas_teorico_practica, nombre, objetivo_general, sigla, id_area_disciplinaria, id_modalidad, id_plan_estudio) Values ('I Ciclo', 4, 0, 0, 0, 4, 0, 'Apoyo de requisito', 'Prueba 4', 'IF0173', 1, 1, 1);
 SET @curso_id = LAST_INSERT_ID();
 
+--referencias_bibliograficas
+INSERT INTO referencia_bibliografica(referencia) Values ('MarieYMarie');
+SET @bibliografia_id = LAST_INSERT_ID();
+
+--curso_correquisitos
+
+INSERT INTO curso_correquisitos(curso_id_curso,correquisitos_id_curso) Values (1,1);
+
+--curso_requisitos
+
+INSERT INTO curso_requisitos(curso_id_curso,requisitos_id_curso) Values (1,1);
+
+--curso_electivos
+
+INSERT INTO curso_electivos (curso_id_curso,electivos_id_curso) Values (1,1);
+
+--curso_referencias_bibliograficas
+
+INSERT INTO curso_referencias_bibliograficas (curso_id_curso,referencias_bibliograficas_id_bibliografia) Values (1,1);
+
+
