62e8767e8f92439a635aabfb127df8fee4bd61d2
Luis Carlos Aguilar Morales <luiscarlos.aguilar@ucr.ac.cr>
2022-06-27 02:27:04 -0600

    Modificaciones a Interfaces de Curso y PlanEstudio

--- src/main/java/cr/ac/ucr/ie/sigie/interfaces/sigiebackend/IPlanEstudio.java
+++ src/main/java/cr/ac/ucr/ie/sigie/interfaces/sigiebackend/IPlanEstudio.java
@@ -2,5 +2,10 @@ package cr.ac.ucr.ie.sigie.interfaces.sigiebackend;
 
 public interface IPlanEstudio {
     int getIdPlanEstudio();
+    int getAnoAprobacion();
+    int getCantidadCiclos();
+    int getCodigoCarrera();
+    int getDuracionAnos();
+
     String getNombreCarrera();
 }
