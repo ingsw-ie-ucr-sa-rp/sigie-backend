e4f9047417240a029da4f2299a5aef997c96bfe1
Ian <ian.ondoy@ucr.ac.cr>
2022-06-28 11:12:48 -0600

    Controller update and method testing
    Added updates to the controller class of PlanEstudios for adding information about it and made some basic test to the API.

--- src/test/java/cr/ac/ucr/ie/sigie/service/PlanEstudioTest.java
+++ src/test/java/cr/ac/ucr/ie/sigie/service/PlanEstudioTest.java
@@ -1,51 +1,48 @@
-package cr.ac.ucr.ie.sigie.controller;
+package cr.ac.ucr.ie.sigie.service;
 
 import cr.ac.ucr.ie.sigie.entity.PlanEstudio;
-import cr.ac.ucr.ie.sigie.service.PlanEstudioService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
 import java.util.List;
 import java.util.NoSuchElementException;
 
-@CrossOrigin
-@RestController
-@RequestMapping(path = "/api/planestudio")
-public class PlanEstudioController {
+@SpringBootTest
+class PlanEstudioTest {
 
-    @Autowired
-    private PlanEstudioService service;
+    PlanEstudioService service = new PlanEstudioService();
+    PlanEstudio planEstudio = new PlanEstudio();
 
+    @Test
     @GetMapping("/planesestudio")
     public List<PlanEstudio> list() {
         return service.listAll();
     }
 
-    @GetMapping("/getPlanEstudio/{id}")
+    @Test
+    @GetMapping("/getPlanEstudio/1") //Parámetro
     public ResponseEntity<PlanEstudio> getPlanEstudioById(@PathVariable Integer id){
         try {
-            PlanEstudio planEstudio = service.get(id);
+            PlanEstudio planEstudio = service.get(1);
             return new ResponseEntity<PlanEstudio>(planEstudio, HttpStatus.OK);
         } catch (NoSuchElementException e) {
             return new ResponseEntity<PlanEstudio>(HttpStatus.NOT_FOUND);
         }
     }
 
+    @Test
     @PostMapping("/savePlanEstudio")
-    public void insertPlanEstudio(PlanEstudio p) { service.save(p);
+    public void insertPlanEstudio(PlanEstudio p) { insertPlanEstudio(planEstudio);
     }
 
-    @PostMapping("/updatePlanEstudio")
+    @Test
+    @PostMapping("/updatePlanEstudio") //Usando id = 1
     public ResponseEntity<PlanEstudio> updatePlanEstudio(@RequestBody PlanEstudio p, @PathVariable Integer id) {
         try {
-            PlanEstudio existingPlanEstudio = service.get(id);
+            PlanEstudio existingPlanEstudio = service.get(1);
             service.save(existingPlanEstudio);
             return new ResponseEntity<PlanEstudio>(p, HttpStatus.OK);
         } catch (NoSuchElementException e) {
@@ -53,9 +50,10 @@ public class PlanEstudioController {
         }
     }
 
-    @DeleteMapping("/deletePlanEstudio/{id}")
+    @Test
+    @DeleteMapping("/deletePlanEstudio/1")
     public void deletePlanEstudio(@PathVariable int id) {
-        service.delete(id);
+        service.delete(1);
     }
 
 }
